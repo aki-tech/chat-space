66205e4f3f0d419b03524e1951af784109fd8f39
aki-tech <gogoakito666@gmail.com>
2020-05-21 17:15:16 +0900

    kok

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-space2020202020202020'
-  config.asset_host = 'https://chat-space2020202020202020.s3.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space2020202020202020'
 end
